09146222ae359a856f49da820e3d931afe36170b
dquitor630 <dquitor630@g.educaand.es>
2023-02-20 23:15:34 +0100

    Ejercicios hasta el 12 terminados

---
+++ src/activity12/Main.java
@@ -0,0 +1,27 @@
+package activity12;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+       new Main().App();
+    }
+
+    void App() throws ClassNotFoundException, SQLException {
+        Scanner keyboard = new Scanner(System.in);
+        String teacher, script;
+        ResultSet rs;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/horario?allowMultiQueries=true", "root", "root");
+        Statement statement = connection.createStatement();
+        System.out.println("escribe el código del profesor que quieres comprobar:");
+        teacher = keyboard.nextLine().toUpperCase();
+        script =  "Select nombre from asignatura join reparto where asignatura.codAsig = reparto.codAsig and codProf = \"" + teacher + "\"";
+        rs = statement.executeQuery(script);
+        while (rs.next()) {
+            System.out.printf("%s\n", rs.getString("nombre"));
+        }
+
+    }
+}
