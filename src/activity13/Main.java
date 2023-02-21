a4f6a46f598b230d88261865bc103344384a4a86
dquitor630 <dquitor630@g.educaand.es>
2023-02-21 21:48:49 +0100

    ejercicio 17 terminado

--- src/activity13/Main.java
+++ src/activity13/Main.java
@@ -2,7 +2,6 @@ package activity13;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
---
+++ src/activity17/Main.java
@@ -0,0 +1,43 @@
+package activity17;
+
+import java.sql.*;
+import java.util.Scanner;
+
+/*DELIMITER //
+CREATE FUNCTION getTeacher(courseId varchar(255), offerId varchar(255))
+RETURNS VARCHAR(255)
+	BEGIN
+			DECLARE nameProf VARCHAR(255);
+			SELECT nombre INTO nameProf
+			FROM curso left join profesor
+			on codTutor = codProf
+			WHERE codCurso = courseId and codOe = offerId;
+			RETURN nameProf;
+	END
+*/
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        new Main().app();
+    }
+
+    void app() throws ClassNotFoundException, SQLException {
+        String type, teacher, course;
+        ResultSet rs;
+        Scanner keyboard = new Scanner(System.in);
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/horario?allowMultiQueries=true", "root", "root");
+        CallableStatement statement = connection.prepareCall("{? = call getTeacher(?,?)}");
+        statement.registerOutParameter(1, Types.VARCHAR);
+        System.out.println("Introduce la oferta educativa");
+        type = keyboard.nextLine();
+        System.out.println("Escribe el código del curso");
+        course = keyboard.nextLine();
+        statement.setString(2, course);
+        statement.setString(3, type);
+        statement.execute();
+        teacher = statement.getString(1);
+        if(teacher != null){
+            System.out.println(teacher);
+        }
+    }
+}
